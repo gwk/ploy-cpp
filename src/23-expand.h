d44513fd79020242b88cade5bef756996a6cabfb
George King <george.w.king@gmail.com>
2014-02-07 17:05:23 -0500

    quasiquote.

--- src/23-expand.h
+++ src/23-expand.h
@@ -4,8 +4,50 @@
 #include "22-env.h"
 
 
-static const BC trace_expand_prefix = "◇";      // white diamond
-static const BC trace_post_expand_prefix = "▫"; // white small square
+static const BC trace_expand_prefix = "◇ ";       // white diamond
+static const BC trace_post_expand_prefix = "▫ ";  // white small square
+
+
+static bool expr_contains_unquote(Obj o) {
+  if (!obj_is_vec(o)) return false;
+  Int len = vec_len(o);
+  Obj* els = vec_els(o);
+  assert(len > 0);
+  if (els[0].u == UNQ.u) return true; // vec is an unquote form.
+  for_imn(i, 1, len) {
+    Obj el = els[i];
+    if (expr_contains_unquote(el)) return true;
+  }
+  return false;
+}
+
+
+static Obj expr_quasiquote(Obj o) {
+  // owns o.
+  if (expr_contains_unquote(o)) { // implies obj_is_vec
+    Int len = vec_len(o);
+    Obj* src = vec_els(o);
+    Obj v = new_vec_raw(len + 2);
+    Obj* dst = vec_els(v);
+    dst[0] = obj_ret_val(CALL);
+    dst[1] = obj_ret_val(Vec);
+    for_in(i, len) {
+      Obj e = src[i];
+      if (obj_is_vec(e) && vec_el(e, 0).u == UNQ.u) { // unquote form
+        check_obj(vec_len(e) == 2, "malformed UNQ form", e);
+        dst[2 + i] = obj_ret(vec_el(e, 1));
+      }
+      else {
+        dst[2 + i] = expr_quasiquote(obj_ret(e));
+      }
+    }
+    obj_rel(o);
+    return v;
+  }
+  else {
+    return new_vec2(obj_ret_val(QUO), o);
+  }
+}
 
 
 static Obj run_call_native(Obj env, Obj func, Int len, Obj* args, Bool is_expand);
@@ -15,6 +57,9 @@ static Obj expand_macro(Obj env, Int len, Obj* args) {
   Obj macro_sym = args[0];
   check_obj(obj_is_sym(macro_sym), "expand argument 0 must be a Sym; found", macro_sym);
   Obj macro = env_get(env, macro_sym);
+  if (macro.u == ILLEGAL.u) { // lookup failed.
+    error_obj("macro lookup error", macro_sym);
+  }
   return run_call_native(env, macro, len - 1, args + 1, true);
 }
 
@@ -30,9 +75,21 @@ static Obj expand(Obj env, Obj code) {
   if (hd.u == QUO.u) {
     return code;
   }
+  if (hd.u == QUA.u) {
+    check_obj(len == 2, "malformed QUA form", code);
+    Obj expr = obj_ret(els[1]);
+    obj_rel(code);
+    return expr_quasiquote(expr);
+  }
   if (hd.u == EXPA.u) {
+#if VERBOSE_EVAL
+    err(trace_expand_prefix); dbg(code);
+#endif
       Obj expanded = expand_macro(env, len - 1, els + 1);
       obj_rel(code);
+#if VERBOSE_EVAL
+    err(trace_post_expand_prefix); dbg(code);
+#endif
       return expand(env, expanded); // macro result may contain more expands; recursively expand.
   }
   else {
