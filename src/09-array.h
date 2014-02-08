b64236324140906771f44a6cbf55890bb51e0e5f
George King <george.w.king@gmail.com>
2014-02-08 18:56:44 -0500

    Array cleanup.

--- src/09-array.h
+++ src/09-array.h
@@ -1,6 +1,8 @@
 // Copyright 2013 George King.
 // Permission to use this file is granted in ploy/license.txt.
 
+// resizable Array type.
+
 #include "08-mem.h"
 
 
@@ -12,27 +14,20 @@ typedef struct {
 #define array0 (Array){.mem=mem0, .cap=0}
 
 
-UNUSED_FN static Bool array_is_valid(Array a) {
-  return mem_is_valid(a.mem) && a.cap >= 0 && a.mem.len <= a.cap;
-}
-
-
-UNUSED_FN static Array array_with_len(Int len) {
-  Array a = array0;
-  a.cap = round_up_to_power_2(len);
-  mem_realloc(&a.mem, a.cap);
-  a.mem.len = len;
-  return a;
+static void assert_array_is_valid(Array* a) {
+  assert(mem_is_valid(a->mem) && a->cap >= 0 && a->mem.len <= a->cap);
 }
 
 
 static void array_grow_cap(Array* a) {
+  assert_array_is_valid(a);
   a->cap = round_up_to_power_2(a->cap + 3); // minimum capacity of 4 elements.
   mem_realloc(&a->mem, a->cap);
 }
 
 
 static Int array_append_move(Array* a, Obj o) {
+  assert_array_is_valid(a);
   if (a->mem.len == a->cap) {
     array_grow_cap(a);
   }
