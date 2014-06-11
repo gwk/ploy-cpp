d087bfe38191a94b3806a46d52f7a1fc8d3cd2b8
George King <george.w.king@gmail.com>
2014-06-11 15:51:07 -0400

    improved rename.sh.

--- sh/rename.sh
+++ sh/rename.sh
@@ -5,3 +5,15 @@ cd $(dirname "$0")/..
 
 # this depends on the rename-enum gloss utility.
 rename-enum -w 2 -off 1 -dash-separator src/*.h
+
+prev_name=''
+for path in $(ls src/*.h src/ploy.c); do
+  if [[ -n "$prev_name" ]]; then
+    echo $path
+    text-replace \
+    -pattern '#include "\d+\w*-[-\w]+.h"' \
+    -replacement "#include \"$prev_name\"" \
+    -modify -no-backup $path
+  fi
+  prev_name="${path#src/}"
+done
