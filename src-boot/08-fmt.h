66d2cf406e75220f3feff5b1f59b9bff5c20e6e6
George King <george.w.king@gmail.com>
2014-07-15 22:58:57 -0400

    add pointer formatting to fix dbg.

--- src-boot/08-fmt.h
+++ src-boot/08-fmt.h
@@ -12,6 +12,7 @@ static void fmt_list_to_file(CFile f, Chars_const fmt, Chars_const args_str, va_
   // the format syntax is similar to printf, but simplified and extended to handle ploy objects.
   // %o: Obj.
   // %i: Int.
+  // %p: Raw.
   // %s: Chars.
   // %c: Char. NOTE: Char arguments must be cast to Uns in the call to align variadic args.
   // for safety and debug convenience, the function takes an additional string argument,
@@ -40,6 +41,7 @@ static void fmt_list_to_file(CFile f, Chars_const fmt, Chars_const args_str, va_
       switch (c) {
         case 'o': write_repr(f, arg); break;
         case 'i': fprintf(f, "%li", arg.i); break;
+        case 'p': fprintf(f, "%p", arg.r); break;
         case 's': fputs(arg.c, f); break;
         case 'c': fputs(char_repr((Char)arg.u), f); break;
         default: error("obj_fmt: invalid placeholder: '%c'; %s", (Uns)c, fmt);
@@ -64,9 +66,9 @@ static void fmt_to_file(CFile f, Chars_const fmt, Chars_const args_str, ...) {
 void dbg(Obj o); // not declared static so that it is always available in debugger.
 void dbg(Obj o) {
   if (obj_is_ref(o)) {
-    errF("%p : %o", o.r, o);
+    errFL("%p : %o", o.r, o);
   } else {
-    errF("%o", o);
+    errFL("%o", o);
   }
   err_flush();
 }
