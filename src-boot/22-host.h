952f4a818fd86fa5893fc0b3fe80c5d416f5b276
George King <george.w.king@gmail.com>
2014-07-24 11:56:25 -0400

    len -> mlen; vec-* -> mem-*.

--- src-boot/22-host.h
+++ src-boot/22-host.h
@@ -132,7 +132,7 @@ static Obj host_dlen(Obj env, Mem args) {
 }
 
 
-static Obj host_len(Obj env, Mem args) {
+static Obj host_mlen(Obj env, Mem args) {
   // owns elements of args.
   assert(args.len == 1);
   Obj o = args.els[0];
@@ -444,7 +444,7 @@ static Obj host_init(Obj env) {
   DEF_FH(2, "ige", host_ige)
   DEF_FH(2, "sym-eq", host_sym_eq)
   DEF_FH(1, "dlen", host_dlen)
-  DEF_FH(1, "len", host_len)
+  DEF_FH(1, "mlen", host_mlen)
   DEF_FH(2, "field", host_field)
   DEF_FH(2, "el", host_el)
   DEF_FH(3, "init-el", host_init_el)
