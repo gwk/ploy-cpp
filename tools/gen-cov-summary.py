139a5d63e1587cb97e3d2ee66a2cc047fce9d4ba
George King <george.w.king@gmail.com>
2014-08-26 22:35:08 -0700

    improve gen-cov-summary.py.

--- tools/gen-cov-summary.py
+++ tools/gen-cov-summary.py
@@ -13,24 +13,36 @@ def errSL(*items):
 
 
 actions = [(re.compile(pat), fmt) for pat, fmt in [
-  (r"Function '(.+)'", '{:24}'),
-  (r'Lines executed:(.+)', 'lines:{:16}'),
-  (r'Branches executed:(.+)', 'branches:{:16}'),
-  (r'Taken at least once:(.+)', 'taken:{:16}'),
+  (r"Function '(.+)'", '{:32}'),
+  (r'Lines executed:(.+%) of (.+)', 'lines: {:>7} of {:>4};    '),
+  (r'Branches executed:(.+%) of (.+)', 'branches: {:>7} of {:>4};    '),
+  (r'Taken at least once:(.+%) of (.+)', 'taken: {:>7} of {:>4}'),
   (r'No calls', ''),
-  (r"File 'src-boot/(.+)", '{:24}'),
+  (r"File 'src-boot/(.+)'", '{:32}'),
   (r'.+:creating .+', ''),
 ]]
 
 
+def print_groups(groups):
+  groups.sort()
+  for g in groups:
+    print(*g)
+
+
 def process_file(f):
+  groups = []
   group = []
+  in_functions = True # functions, then files.
   for line_raw in f:
     line = line_raw.strip()
     if not line:
-      print(*group)
+      groups.append(group)
       group = []
       continue
+    if in_functions and line.startswith('File'): # transition.
+      print_groups(groups) # functions.
+      groups = []
+      in_functions = False
     for r, fmt in actions:
       m = r.fullmatch(line)
       if m:
@@ -38,6 +50,7 @@ def process_file(f):
         break
     if line:
       group.append(line)
+  print_groups(groups) # files.
 
 with open(sys.argv[1]) as f:
   process_file(f)
