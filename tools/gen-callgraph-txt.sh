17429ff2480d9d2a4fbe266d226a7615cb1f3ea5
George King <george.w.king@gmail.com>
2015-11-15 13:25:37 -0500

    build scripts bail on error.

--- tools/gen-callgraph-txt.sh
+++ tools/gen-callgraph-txt.sh
@@ -5,6 +5,8 @@
 # opt 3.4.1 outputs raw llvm symbols, some of which contain 0x01 bytes (ascii SOH).
 # expose these with cat -v, which displays non-printing characters.
 
+set -e
+
 src=$1
 dst=$2
 
